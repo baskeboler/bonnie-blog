0b2b9788bb5bda122ee0b641cc288931b9ed8efe
Victor Gil <baskeboler@gmail.com>
2019-06-07 07:19:59 -0300

    fn

--- src/lambda/person.js
+++ src/lambda/person.js
@@ -1,27 +1,13 @@
-import http from 'http';
+exports.handler = async (event, context) => {
 
-// For more info, check https://www.netlify.com/docs/functions/#javascript-lambda-functions
-exports.handler = (event, context, callback) => {
-    console.log('queryStringParameters', event.queryStringParameters)
-    var buffers = []
-    http.get('http://thispersondoesnotexist.com/image', (res) => {
-        res.on('end', function () {
-            callback(null, {
-                // return null to show no errors
-                statusCode: 200, // http status code
-                body: buffers,
-            })
-        })
+    const redirectUrl = 'https://thispersondoesnotexist.com/image'
 
-        res.on('data', function (data) {
-            buffers.push(data)
-        })
-    });
-
+    return {
+        statusCode: 302,
+        headers: {
+            Location: redirectUrl,
+            'Cache-Control': 'no-cache',
+        },
+        body: JSON.stringify({})
+    }
 }
-
-// Now you are ready to access this API from anywhere in your Gatsby app! For example, in any event handler or lifecycle method, insert:
-// fetch("/.netlify/functions/hello")
-//    .then(response => response.json())
-//    .then(console.log)
-// For more info see: https://www.gatsbyjs.org/blog/2018-12-17-turning-the-static-dynamic/#static-dynamic-is-a-spectrum
